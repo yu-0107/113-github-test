693aa4576a1e19c5401633d00514270055492071
onion <a0966550619@gmail.com>
2024-10-14 11:40:04 +0800

    完成菱形的進階練習

--- pra03.php
+++ pra03.php
@@ -200,7 +200,7 @@ for($i=0;$i<6;$i++){
 <h2>矩形練習</h2>
 <?php
 $width=7;
-
+//套用變數更彈性
 for($i=0;$i<$width;$i++){
     for($j=0;$j<$width;$j++){
         if($i==0 || $i==($width-1)){
@@ -213,7 +213,90 @@ for($i=0;$i<$width;$i++){
     }
     echo "<br>";
 }
-
 ?>
+<hr>
+<h2>矩形對角線</h2>
+<?php
+$width=7;
+//套用變數更彈性
+for($i=0;$i<$width;$i++){
+    for($j=0;$j<$width;$j++){
+        if($i==0 || $i==($width-1)){
+            echo "*";
+        }else if($j==0 ||$j==($width-1)){
+            echo "*";
+        }else if($i==$j ||$j==($width-1-$i)){
+            echo "*";    
+        }else{
+            echo "&nbsp;";
+        }
+    }
+    echo "<br>";
+}
+?>
+<hr>
+<h2>菱形練習</h2>
+<?php
+$size=11;
+
+if($size >=3){
+    if($size%2==0){
+        $size++;
+    }
+    
+}else{
+    echo "數字太小,無法畫出菱形<br>";
+}
+
+for($i=0;$i<$size;$i++){
+    if($i>floor($size/2)){
+        for($k=0;$k<$i-floor($size/2);$k++){
+            echo "&nbsp;";
+            //空格的寫法
+    }
+        for($j=0;$j<2*($i-(2*($i-floor($size/2))))+1;$j++){
+            echo "*";
+
+    }
+        echo "<br>";
+}else{    
+        for($k=0;$k<floor($size/2)-$i;$k++){
+            echo "&nbsp;";
+            //空格的寫法
+    }
+        for($j=0;$j<(2*$i+1);$j++){
+            echo "*";
+
+    }
+    echo "<br>";
+}}
+?>
+<hr>
+<h2>菱形對角線練習</h2>
+<?php
+for($i=0;$i<$size;$i++){
+    if($i>(floor($size/2))){
+        $k1=$i-(floor($size/2));
+        $j1=2*($i-(2*($i-(floor($size/2)))))+1;
+    }else{
+        $k1=(floor($size/2))-$i;
+        $j1=(2*$i+1);
+    }
+
+    for($k=0;$k<$k1;$k++){
+        echo "&nbsp;";
+    }
+
+    for($j=0;$j<$j1;$j++){
+        if($j==0 || $j==$j1-1 || $i==(floor($size/2)) || $j==floor(($j1-1)/2)){
+            echo "*";
+        }else{
+            echo "&nbsp;";
+        }        
+    }
+    echo "<br>";
+}
+?>
+
 </body>
 </html>
