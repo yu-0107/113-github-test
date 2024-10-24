8f4ff4b4579c61dd83fca9e1ddbe09805b1c3aa4
onion <a0966550619@gmail.com>
2024-10-24 16:35:19 +0800

    根據不同日期條件加上css

--- date.php
+++ date.php
@@ -147,6 +147,18 @@ for($i=0;$i<5;$i++){
         text-align: center;
         border:1px solid #999;
     }
+    .holiday{
+        background:pink;
+        color:#999;
+    }
+    .grey-text{
+        color:#999;
+    }
+    .today{
+        background:blue;
+        color:white;
+
+    }
 </style>
 <h3><?php echo date("m月");?></h3> 
 <table>
@@ -176,6 +188,55 @@ for($i=0;$i<6;$i++){
             echo $dayNum;
         }
         echo "</td>";
+    }
+    echo "</tr>";
+}
+
+
+
+
+
+
+?>
+</table>
+
+<h3><?php echo date("m月");?></h3> 
+<table>
+<tr>
+    <td></td>
+    <td>日</td>
+    <td>一</td>
+    <td>二</td>
+    <td>三</td>
+    <td>四</td>
+    <td>五</td>
+    <td>六</td>
+</tr>
+<?php
+$firstDay=date("Y-m-1");
+$firstDayTime=strtotime($firstDay);
+$firstDayWeek=date("w",strtotime(date("Y-m-1")));
+
+for($i=0;$i<6;$i++){
+    echo "<tr>";
+    echo "<td>";
+    echo $i+1;
+    echo "</td>";
+    for($j=0;$j<7;$j++){
+        // echo "<td class='holiday'>";
+        $cell=$i*7+$j -$firstDayWeek;
+        $theDayTime=strtotime("$cell days".$firstDay);
+
+        // 所需要式css判斷
+        $theMonth=(date("m",$theDayTime)==date("m"))?'':'grey-text';
+        $isToday=(date("Y-m-d",$theDayTime)==date("Y-m-d"))?'today':'';
+        $w=date("w",$theDayTime);
+        $isHoliday=($w==0 || $w==6)?'holiday':'';
+
+        echo "<td class='$isHoliday $theMonth $isToday'>";
+        echo date("d",$theDayTime);
+        echo "</td>";
+        
     }
     echo "</tr>";
 }
